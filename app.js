834d7d45e2b8496ce543347cd5d0fb330f8024c3
Lucia Draesner <alexdraesner@mac.localdomain>
2026-03-30 20:12:50 +0200

    we worked through the tut at this point now we are only adding stuff we want on our blog.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const { logger } = require('./middleware/logger.js')
 const mongoose = require('mongoose')
@@ -5,14 +7,13 @@ const Post = require('./models/post.js')
 
 const app = express()
 
-mongoose.connect('mongodb://127.0.0.1:27017/alexblog')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('💽 Database connected'))
   .catch(error => console.error(error))
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 
-const PORT = 3000
 
 app.use('/blog', express.static('public'))
 app.use(logger)
@@ -39,8 +40,27 @@ app.get('/posts', async (request, response) => {
     }
 })
 
-app.get('/contact', (request, response) => {
-    response.sendFile('contact.html', { root: 'public' })
+app.get('/posts/:slug/edit', async (request, response) => {
+    try {
+        const slug = request.params.slug
+        const post = await Post.findOne({ slug: slug }).exec()
+        if(!post) throw new Error('Post not found')
+
+        response.render('posts/edit', { post: post })
+    } catch(error) {
+        console.error(error)
+        response.status(404).send('Could not find the post you\'re looking for.')
+    }
+})
+
+app.get('/posts/:slug/delete', async (request, response) => {
+    try {
+        await Post.findOneAndDelete({ slug: request.params.slug })
+        response.redirect('/posts')
+    } catch(error) {
+        console.error(error)
+        response.send('Error: The post could not be deleted.')
+    }
 })
 
 app.get('/posts/:slug', async (request, response) => {
@@ -54,6 +74,10 @@ app.get('/posts/:slug', async (request, response) => {
         console.error(error)
         response.status(404).send('Could not find the post you\'re looking for.')
     }
+})
+
+app.get('/contact', (request, response) => {
+    response.sendFile('contact.html', { root: 'public' })
 })
 
 app.post('/contact', (request, response) => {
@@ -78,6 +102,27 @@ app.post('/posts', async (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`Started server on port ${PORT}`)
+app.post('/posts/:slug', async (request, response) => {
+    try {
+        const post = await Post.findOneAndUpdate(
+            { slug: request.params.slug },
+            {
+                title: request.body.title,
+                slug: request.body.slug,
+                description: request.body.description,
+                content: request.body.content,
+                isPublished: request.body.isPublished === 'true'
+            },
+            { new: true }
+        )
+        response.redirect(`/posts/${post.slug}`)
+    } catch(error) {
+        console.error(error)
+        response.send('Error: The post could not be updated.')
+    }
 })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Started server on port ${process.env.PORT}`)
+})
+
